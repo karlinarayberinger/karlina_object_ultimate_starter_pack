8220461e6328efa8b251c26f8bbfea281181b387
Karlina Ray Beringer <karlina.ray.beringer@protonmail.com>
2022-10-28 19:49:40 -0700

    Add files via upload

---
+++ eulers_number_approximation.cpp
@@ -0,0 +1,18 @@
+/**
+ * @file: eulers_number_approximation.cpp
+ * @type: C++ (source file)
+ * @date: 01_AUGUST_2022
+ * @author: Karlina Ray Beringer
+ * @license: PUBLIC_DOMAIN
+ */
+
+/* preprocessing directives */
+#include <iostream> // command line input and output
+#include <fstream> // file input and output
+#define MAXIMUM_N 1000 // constant which represents maximum N value
+
+/* program entry point */
+int main()
+{
+	return 0;
+}
